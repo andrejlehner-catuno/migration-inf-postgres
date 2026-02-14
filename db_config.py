eb885a746b7be187667ca0fd4c87eaa9cdec134f
Andrej Lehner <andrej.lehner@catuno.de>
2026-02-14 01:21:42 +0100

    db_config adaptation

--- db_config.py
+++ db_config.py
@@ -1,33 +1,38 @@
 import os
-import sys
 import jaydebeapi
 import psycopg2
 
-def get_credentials():
-    ifx_pw = os.getenv('IFX_PW')
-    pg_pw = os.getenv('PG_PW')
-    
-    if not ifx_pw or not pg_pw:
-        print("❌ FEHLER: Datenbank-Passwörter (IFX_PW/PG_PW) nicht gefunden!")
-        sys.exit(1)
-    return ifx_pw, pg_pw
+# Falls eine Variable fehlt, wirft os.environ[key] sofort einen KeyError
+# Das ist genau das "Hart-Abbrechen", das wir wollen.
+PG_PASSWORD = os.environ['PG_PW']
+IFX_PASSWORD = os.environ['IFX_PW']
 
-def connect_informix():
-    ifx_pw, _ = get_credentials()
-    url = "jdbc:informix-sqli://localhost:9095/unostdtest:INFORMIXSERVER=ol_catuno_utf8en;CLIENT_LOCALE=en_US.utf8;DB_LOCALE=en_US.utf8;DBDATE=DMY4.;DBMONEY=.;DBDELIMITER=|"
-    driver = "com.informix.jdbc.IfxDriver"
-    jars = [
-        r"C:\baustelle_8.6\de.cerpsw.barracuda.runtime\lib\de.cerpsw.sysfunction\jdbc-4.50.11.jar",
-        r"C:\baustelle_8.6\de.cerpsw.barracuda.runtime\lib\de.cerpsw.sysfunction\bson-3.8.0.jar"
-    ]
-    return jaydebeapi.connect(driver, url, ["informix", ifx_pw], jars)
+# Zentrale PostgreSQL Config
+PG_CONFIG = {
+    'host': 'localhost',
+    'port': 5432,
+    'database': 'catuno_production',
+    'user': 'catuno',
+    'password': PG_PASSWORD
+}
+
+# Informix JDBC Details
+INFORMIX_JDBC_URL = "jdbc:informix-sqli://localhost:9095/unostdtest:INFORMIXSERVER=ol_catuno_utf8en;CLIENT_LOCALE=en_US.utf8;DB_LOCALE=en_US.utf8;DBDATE=DMY4.;DBMONEY=.;DBDELIMITER=|"
+INFORMIX_JDBC_DRIVER = "com.informix.jdbc.IfxDriver"
+INFORMIX_JDBC_JAR = [
+    r"C:\baustelle_8.6\de.cerpsw.barracuda.runtime\lib\de.cerpsw.sysfunction\jdbc-4.50.11.jar",
+    r"C:\baustelle_8.6\de.cerpsw.barracuda.runtime\lib\de.cerpsw.sysfunction\bson-3.8.0.jar"
+]
 
 def connect_postgres():
-    _, pg_pw = get_credentials()
-    return psycopg2.connect(
-        host='localhost',
-        port=5432,
-        database='catuno_production',
-        user='catuno',
-        password=pg_pw
+    """Verbindung zu PostgreSQL mit den Jenkins-Secrets"""
+    return psycopg2.connect(**PG_CONFIG)
+
+def connect_informix():
+    """Verbindung zu Informix mit den Jenkins-Secrets"""
+    return jaydebeapi.connect(
+        INFORMIX_JDBC_DRIVER,
+        INFORMIX_JDBC_URL,
+        ["informix", IFX_PASSWORD],
+        INFORMIX_JDBC_JAR
     )
